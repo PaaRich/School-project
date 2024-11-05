8fe636d0220b2ebe3066e058f810efbe7ac68656
PaaRich <siehrichmond50@gmail.com>
2024-11-05 02:19:04 +0000

    extended tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "engineering-students": "url('/public/engineering_student.jpg')",
+        university: "url('/public/university_building.jpg')",
+      },
+    },
   },
   plugins: [],
 };
